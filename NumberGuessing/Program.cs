2db2ffb0b9165d612d34685ccaa79ebe660ee08d
Nejra <121252576+nejrap@users.noreply.github.com>
2023-03-25 15:26:11 +0100

    Guessing Game in C#
    Create a Number Guessing Game C#

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random num = new Random();
+        int minNum = num.Next(0, 100);
+
+        bool win = false;
+
+        do
+        {
+            Console.WriteLine("Quess a number in range 0 and 100");
+            string a = Console.ReadLine();
+
+            int i = int.Parse(a);
+
+            if(i > minNum)
+            {
+                Console.WriteLine("Too high");
+            } else if (i<minNum) {
+                Console.WriteLine("Too low");
+
+            } else if (i == minNum)
+            {
+                Console.WriteLine("Congrats! You wiiiiin! ");
+                win = true;
+            }
+
+        } while (win == false);
+
+        Console.WriteLine("Thanks for playing. :)");
+
+    }
+}
